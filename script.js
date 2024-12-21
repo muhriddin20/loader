7c31f481066bca5112c74f86f8211d20418d697c
Davlatov Muhriddin <162937360+davlatov-muhriddin1@users.noreply.github.com>
2024-12-21 05:51:40 +0500

    loader

---
+++ script.js
@@ -0,0 +1,19 @@
+const percent = document.querySelector(".percent");
+const progress = document.querySelector(".progress");
+
+let count = 4;
+let per = 16;
+let loading = setInterval(animate, 50);
+
+function animate() {
+  if (count == 100 && per == 400) {
+    percent.classList.add("text-blink");
+    text.style.display = "block";
+    clearInterval(loading);
+  } else {
+    per = per + 4;
+    count = count + 1;
+    progress.style.width = `${per}px`;
+    percent.textContent = `${count}%`;
+  }
+}
